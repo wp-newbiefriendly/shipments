9df3b2cce1eeeac18c9762e1fa967a5185ff96f4
wp-newbiefriendly <locokoks@gmail.com>
2025-11-25 12:01:22 +0100

    #27.22 - dodavanje permalink shipments

---
+++ app/Http/Requests/NewShipmentRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class NewShipmentRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:128',
+            'from_city' => 'required|string|max:64',
+            'from_country' => 'required|string|max:64',
+            'to_city' => 'required|string|max:64',
+            'to_country' => 'required|string|max:64',
+            'price' => 'required|numeric|min:0',
+            'status' => 'required|in:in_progress,unassigned,completed,problem',
+            'user_id' => 'required|exists:users,id',
+            'details' => 'nullable|string',
+        ];
+    }
+
+}
---
+++ database/migrations/2025_11_24_095540_set_default_status_to_shipments.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Shipments;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('shipments', function (Blueprint $table) {
+            $table->string('status', 20)
+                ->default(Shipments::STATUS_UNASSIGNED)
+                ->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('shipments', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+};
