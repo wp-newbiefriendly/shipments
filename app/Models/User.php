995bda4407e5cd0d2ef04a4174910cc436dca293
wp-newbiefriendly <locokoks@gmail.com>
2026-02-06 10:08:40 +0100

    #30.08 - Roles - Add role

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,33 +11,32 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
+    const ROLE_CLIENT = 'client';
+    const ROLE_ADMINISTRATOR = 'administrator';
+    const ROLE_TRUCKER = 'trucker';
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    const ALLOWED_ROLES = [
+        self::ROLE_CLIENT, self::ROLE_ADMINISTRATOR, self::ROLE_TRUCKER
+    ];
+
+    public function setRoleAttribute(string $role)
+    {
+        if (!in_array($role, self::ALLOWED_ROLES)) {
+            throw new \InvalidArgumentException('Invalid role');
+        }
+        $this->attributes['role'] = $role;
+    }
+
     protected function casts(): array
     {
         return [
