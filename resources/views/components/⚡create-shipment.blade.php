1456b63fdbb43cac7638b3e63b4207bfda42b720
wp-newbiefriendly <locokoks@gmail.com>
2026-02-11 10:23:25 +0100

    #33.5 - Create shipment livewire

--- resources/views/components/⚡create-shipment.blade.php
+++ resources/views/components/⚡create-shipment.blade.php
@@ -4,10 +4,49 @@ use Livewire\Component;
 
 new class extends Component
 {
-    //
+   public string $title;
+
+   public string $fromCity;
+
+   public string $fromCountry;
+
+   public string $toCity;
+
+   public string $toCountry;
+
 };
 ?>
 
 <div>
-    {{-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius --}}
+
+    <form>
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control"
+                   wire:model="title" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="from_city" class="form-label">From City</label>
+            <input type="text" class="form-control" wire:model="fromCity" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="from_country" class="form-label">From Country</label>
+            <input type="text" class="form-control" wire:model="fromCountry" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="to_city" class="form-label">To City</label>
+            <input type="text" class="form-control" wire:model="toCity" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="to_country" class="form-label">To Country</label>
+            <input type="text" class="form-control" wire:model="toCountry" required>
+        </div>
+
+    </form>
+
+
 </div>
